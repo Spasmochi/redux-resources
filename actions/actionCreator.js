bcd264b036dd6c0d5b34640e02b9335b60eb7f8d
Spas <12554483+Spasmochi@users.noreply.github.com>
2020-09-30 13:23:29 +1000

    added actions

---
+++ actions/actionCreator.js
@@ -0,0 +1,17 @@
+export const increment = (index) => ({
+  type: "INCREMENT_LIKES",
+  index,
+});
+
+export const addComment = ((resourceId, author, comment) = {
+  type: "ADD_COMMENT",
+  resourceId,
+  author,
+  comment,
+});
+
+export const removeComment = (resourceId, i) => ({
+  type: "REMOVE_COMMENT",
+  i,
+  resourceId,
+});
